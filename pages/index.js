5e292eed5b6f216a6cf9f2917084135a9b6807bc
unknown <pedrorembold@gmail.com>
2022-01-25 21:34:53 -0300

    Segunda aula Imersão react. ainda falta o desafio do Paulo

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,8 @@
 
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json'
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-        `}</style>
-    )
-}
 
 function Titulo(props) {
     const Tag = props.tag;
@@ -61,11 +34,13 @@ function Titulo(props) {
 //  export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'peas';
+    //const username = 'peas';
+    const [username, setUsername] = React.useState('omariosouto');
+    const roteamento = useRouter();
+    
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -92,6 +67,12 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (infosDoEvento) {
+                            infosDoEvento.preventDefault();
+                            console.log('Alguém submeteu o form')
+                            roteamento.push('/chat');
+
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -102,17 +83,41 @@ export default function PaginaInicial() {
                             {appConfig.name}
                         </Text>
 
-                        <TextField
-                            fullWidth
-                            textFieldColors={{
-                                neutral: {
-                                    textColor: appConfig.theme.colors.neutrals[200],
-                                    mainColor: appConfig.theme.colors.neutrals[900],
-                                    mainColorHighlight: appConfig.theme.colors.primary[500],
-                                    backgroundColor: appConfig.theme.colors.neutrals[800],
-                                },
-                            }}
-                        />
+                        {/*<input
+                            type="text" 
+                            value={username}
+                            onChange={function (event){
+                                console.log('usuario digitou', event.target.value)
+                                //onde está o valor?
+                                const valor = event.target.value;
+                                //Trocar o valor da variável
+                                //através do React e avise quem precisa
+                                setUsername(valor);
+                            }
+                        }
+                    /> */}
+                            <TextField
+                                value={username}
+                                onChange={function (event){
+                                    //Bloquear a foto antes de 3 caracteres
+                                    event.target.minLength = 3;
+                                    //onde está o valor?
+                                    const valor = event.target.value;
+                                    //Trocar o valor da variável
+                                    //através do React e avise quem precisa
+                                    setUsername(valor);
+                                }
+                            }
+                                fullWidth
+                                textFieldColors={{
+                                    neutral: {
+                                        textColor: appConfig.theme.colors.neutrals[200],
+                                        mainColor: appConfig.theme.colors.neutrals[900],
+                                        mainColorHighlight: appConfig.theme.colors.primary[402],
+                                        backgroundColor: appConfig.theme.colors.neutrals[800],
+                                    },
+                                }}
+                            />
                         <Button
                             type='submit'
                             label='Entrar'
@@ -121,7 +126,7 @@ export default function PaginaInicial() {
                                 contrastColor: appConfig.theme.colors.neutrals["050"],
                                 mainColor: appConfig.theme.colors.primary[300],
                                 mainColorLight: appConfig.theme.colors.primary[400],
-                                mainColorStrong: appConfig.theme.colors.primary[600],
+                                mainColorStrong: appConfig.theme.colors.primary[401],
                             }}
                         />
                     </Box>
